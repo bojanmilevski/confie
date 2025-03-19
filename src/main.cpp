58686755062428bbd0113d92dd88cc2b58234711
Bojan Milevski <bojan@bojanmilevski.com>
2025-03-19 18:28:10 +0100

    debug build; minor improvements to toml parsing

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 #include <cstdlib>
 #include <expected>
 #include <filesystem>
-#include <iostream>
 #include <print>
 #include <range/v3/range.hpp>
 #include <set>
 #include <string>
 #include <string_view>
 #include <toml++/toml.hpp>
+
+#ifdef DEBUG
+#include <iostream>
+#endif
 
 enum ErrorType {
   CLI_PARSE,
@@ -23,6 +26,10 @@ enum ErrorType {
 
 struct Error {
 public:
+  Error() = delete;
+  //  Error(const Error&) = delete;
+  Error& operator=(const Error&) = delete;
+
   static auto create(const ErrorType&& error_type, const std::string_view&& message) noexcept
       -> std::unexpected<Error> {
     return std::unexpected(Error(std::move(error_type), std::move(message)));
@@ -31,8 +38,6 @@ public:
   [[nodiscard]] auto get_message() const noexcept -> std::string_view { return m_message; }
 
   [[nodiscard]] auto get_error() const noexcept -> ErrorType { return m_error_type; }
-
-  [[nodiscard]] explicit Error() noexcept = delete;
 
 private:
   const ErrorType m_error_type;
@@ -46,18 +51,23 @@ template <typename R> using expected = std::expected<R, Error>;
 
 struct Group {
 public:
-  [[nodiscard]] explicit Group() noexcept = delete;
+  Group() = delete;
+  // Group(const Group&) = delete;
+  Group& operator=(const Group&) = delete;
+  ~Group() = default;
 
-  static auto create(const std::string_view&& name, const std::filesystem::path&& destination,
-                     const std::set<std::filesystem::path>&& include,
-                     const std::optional<std::set<std::filesystem::path>>&& exclude,
-                     const std::optional<std::set<std::filesystem::path>>&& archive,
-                     const std::optional<std::set<std::filesystem::path>>&& protect) noexcept -> Group {
-    return Group(std::move(name), std::move(destination), std::move(include), std::move(exclude), std::move(archive),
-                 std::move(protect));
-  }
+  [[nodiscard]] explicit Group(const std::string_view&& name, const std::filesystem::path&& destination,
+                               const std::set<std::filesystem::path>&& include,
+                               const std::optional<std::set<std::filesystem::path>>&& exclude,
+                               const std::optional<std::set<std::filesystem::path>>&& archive,
+                               const std::optional<std::set<std::filesystem::path>>&& protect) noexcept
+      : m_name{std::move(name)}, m_destination{std::move(destination)}, m_include{std::move(include)},
+        m_exclude{std::move(exclude)}, m_archive{std::move(archive)}, m_protect{std::move(protect)} {}
 
-  [[nodiscard]] const auto print() const noexcept {
+  const bool operator<(const Group& other) const { return m_name < other.m_name; }
+
+#ifdef DEBUG
+  [[nodiscard]] const auto print() const& noexcept {
     std::cout << "Name: " << m_name << '\n';
     std::cout << "Destination: " << m_destination << '\n';
 
@@ -75,6 +85,7 @@ public:
       std::ranges::for_each(*m_protect, [&](const auto& entry) { std::cout << "Protect: " << entry << '\n'; });
     }
   }
+#endif
 
 private:
   const std::string_view m_name;
@@ -83,14 +94,6 @@ private:
   const std::optional<std::set<std::filesystem::path>> m_exclude;
   const std::optional<std::set<std::filesystem::path>> m_archive;
   const std::optional<std::set<std::filesystem::path>> m_protect;
-
-  [[nodiscard]] explicit Group(const std::string_view&& name, const std::filesystem::path&& destination,
-                               const std::set<std::filesystem::path>&& include,
-                               const std::optional<std::set<std::filesystem::path>>&& exclude,
-                               const std::optional<std::set<std::filesystem::path>>&& archive,
-                               const std::optional<std::set<std::filesystem::path>>&& protect)
-      : m_name{std::move(name)}, m_destination{std::move(destination)}, m_include{std::move(include)},
-        m_exclude{std::move(exclude)}, m_archive{std::move(archive)}, m_protect{std::move(protect)} {}
 
   // FIX:: std::ranges::transform
   [[nodiscard]] auto iterate(const std::filesystem::path&& entry) const noexcept -> std::set<std::filesystem::path> {
@@ -119,7 +122,9 @@ private:
 
 struct Configuration {
 public:
-  [[nodiscard]] explicit Configuration() noexcept = delete;
+  Configuration() = delete;
+  // Configuration(const Configuration&) = delete;
+  Configuration& operator=(const Configuration&) = delete;
 
   [[nodiscard]] static auto create(const std::optional<std::filesystem::path>&& path) noexcept
       -> expected<Configuration> {
@@ -139,59 +144,61 @@ public:
       return Error::create(NOT_FILE, "Configuration is not a file.");
     }
 
-    const auto toml_file = toml::parse_file(config_file_path.c_str());
-    if (!toml_file.contains("group")) {
-      return Error::create(PLACEHOLDER,
-                           "Groups missing in configuration file"); // FIX: propagate error, dont create a new one
+    const auto toml = toml::parse_file(config_file_path.c_str());
+    if (!toml) {
+      return Error::create(PLACEHOLDER, "Unable to parse toml file");
     }
 
-    const auto t_groups = toml_file["group"].as_array();
+    if (!toml.table().contains("group")) {
+      return Error::create(PLACEHOLDER, "Groups missing in configuration file");
+    }
+
+    const auto t_groups = toml["group"].as_array();
     if (!t_groups) {
-      return Error::create(PLACEHOLDER, "groupS"); // FIX: propagate error, dont create a new one
+      return Error::create(PLACEHOLDER, "groupS");
     }
 
-    std::vector<Group> groups{};
+    std::set<Group> groups{};
     t_groups->for_each([&](const auto& group) -> expected<void> {
       const auto table = group.as_table();
       if (!table) {
-        return Error::create(PLACEHOLDER, "group"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "group");
       }
 
-      // dereferencing means extracting the value from toml::parse_result
       const auto name = (*table)["name"].as_string();
       if (!name) {
-        return Error::create(PLACEHOLDER, "name"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "name");
       }
 
       const auto t_destination = (*table)["destination"].as_string();
       if (!t_destination) {
-        return Error::create(PLACEHOLDER, "destination"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "destination");
       }
 
-      const auto destination = parse_tilde(**t_destination);
+      const auto destination = parse_path(std::move(**t_destination));
 
-      const auto include = parse_array_field(*table, std::move("include"));
+      const auto include = parse_array_field(*table, "include");
       if (!include) {
-        return Error::create(PLACEHOLDER, "include"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "include");
       }
 
-      const auto exclude = parse_optional_array_field(*table, std::move("exclude"));
+      const auto exclude = parse_optional_array_field(*table, "exclude");
       if (!exclude) {
-        return Error::create(PLACEHOLDER, "exclude"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "exclude");
       }
 
-      const auto archive = parse_optional_array_field(*table, std::move("archive"));
+      const auto archive = parse_optional_array_field(*table, "archive");
       if (!archive) {
-        return Error::create(PLACEHOLDER, "archive"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "archive");
       }
 
-      const auto protect = parse_optional_array_field(*table, std::move("protect"));
+      const auto protect = parse_optional_array_field(*table, "protect");
       if (!protect) {
-        return Error::create(PLACEHOLDER, "protect"); // FIX: propagate error, dont create a new one
+        return Error::create(PLACEHOLDER, "protect");
       }
 
-      groups.push_back(Group::create(std::move(**name), std::move(destination), std::move(*include),
-                                     std::move(*exclude), std::move(*archive), std::move(*protect)));
+      groups.insert(Group(std::move(**name), std::move(destination), std::move(*include), std::move(*exclude),
+                          std::move(*archive), std::move(*protect)));
 
       return {};
     });
@@ -199,16 +206,18 @@ public:
     return Configuration(std::move(groups));
   }
 
-  auto print() const noexcept -> void {
-    std::ranges::for_each(m_groups, [&](auto& entry) { entry.print(); });
+#ifdef DEBUG
+  auto print() const& noexcept -> void {
+    std::ranges::for_each(m_groups, [&](const auto& entry) { entry.print(); });
   }
+#endif
 
 private:
-  const std::vector<Group> m_groups{}; // FIX: std::set
+  const std::set<Group> m_groups{};
 
-  [[nodiscard]] explicit Configuration(const std::vector<Group>&& groups) noexcept : m_groups(std::move(groups)) {}
+  [[nodiscard]] explicit Configuration(const std::set<Group>&& groups) noexcept : m_groups(std::move(groups)) {}
 
-  [[nodiscard]] auto static parse_optional_array_field(const auto& table, const std::string_view&& name) noexcept
+  [[nodiscard]] auto static parse_optional_array_field(const toml::table& table, const std::string_view&& name) noexcept
       -> expected<std::optional<std::set<std::filesystem::path>>> {
     if (!table.contains(name)) {
       return {};
@@ -217,7 +226,7 @@ private:
     return parse_array_field(table, std::move(name));
   }
 
-  [[nodiscard]] auto static parse_array_field(const auto& table, const std::string_view&& name) noexcept
+  [[nodiscard]] auto static parse_array_field(const toml::table& table, const std::string_view&& name) noexcept
       -> expected<std::set<std::filesystem::path>> {
     const auto t_entry = table[name].as_array();
     if (!t_entry) {
@@ -226,27 +235,27 @@ private:
 
     std::set<std::filesystem::path> paths{};
     t_entry->for_each([&](const auto& element) {
-      const auto path = **element.as_string();
-      std::filesystem::path p{};
-
-      if (path.length() > 0) {
-        if (path.at(0) == '~') {
-          p = parse_tilde(path);
-        } else {
-          p = std::filesystem::path(path);
-        }
+      const auto string = **element.as_string(); // FIX: check
+      if (string.length() > 0) {
+        const auto path = parse_path(std::move(string));
+        paths.insert(std::move(path));
       }
-
-      paths.insert(p);
     });
 
     return paths;
   }
 
-  auto static parse_tilde(const std::string& path) noexcept -> std::filesystem::path {
+  [[nodiscard]] auto static parse_path(const std::string&& path) noexcept -> std::filesystem::path {
+    if (path.at(0) == '~') {
+      return parse_tilde(std::move(path));
+    }
+
+    return {path};
+  }
+
+  [[nodiscard]] auto static parse_tilde(const std::string&& path) noexcept -> std::filesystem::path {
     const static auto home = std::getenv("HOME");
-    const auto abs_path = home + path.substr(1);
-    return std::filesystem::path{abs_path};
+    return {home + path.substr(1)};
   }
 };
 
@@ -267,7 +276,9 @@ auto main(const int argc, const char* argv[]) noexcept -> int {
     std::exit(config.error().get_error());
   }
 
+#ifdef DEBUG
   config->print();
+#endif
 
   return EXIT_SUCCESS;
 }
